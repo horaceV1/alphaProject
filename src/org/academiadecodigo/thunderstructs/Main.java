2d2b5eee73dfffaca500ff72990e77c073a544cc
Vasco Sousa <az.thewolf.jr@gmail.com>
2019-11-07 22:04:52 -0100

    Menu is now functional

--- src/org/academiadecodigo/thunderstructs/Main.java
+++ src/org/academiadecodigo/thunderstructs/Main.java
@@ -1,13 +1,15 @@
 package org.academiadecodigo.thunderstructs;
 
-import org.academiadecodigo.bootcamp.Prompt;
-import org.academiadecodigo.bootcamp.scanners.menu.MenuInputScanner;
-
 public class Main {
+
+
     public static void main(String[] args) {
+
         System.out.println("Welcome to Alpha Project");
+
         Menu menu = new Menu();
-        menu.menu();
+        menu.run();
+
     }
 
 }
--- src/org/academiadecodigo/thunderstructs/Menu.java
+++ src/org/academiadecodigo/thunderstructs/Menu.java
@@ -5,13 +5,40 @@ import org.academiadecodigo.bootcamp.scanners.menu.MenuInputScanner;
 
 public class Menu {
 
-    public void menu(){
+    public void run(){
         Prompt prompt = new Prompt(System.in, System.out);
 
         String[] options = {"Instructions", "Number Picker", "Quit"};
 
         MenuInputScanner menu = new MenuInputScanner(options);
-        menu.setMessage("Menu");
+        menu.setMessage("Pick a number: ");
+
         int menuAnswer = prompt.getUserInput(menu);
+        System.out.println("User chose: " + options[menuAnswer - 1]);
+
+        menuOptions(menuAnswer - 1);
+
     }
+
+    public void menuOptions(int menuAnswer) {
+
+        while (true) {
+            switch (menuAnswer) {
+                case 1:
+                    System.out.println("Instructions chosen");
+                    System.exit(0);
+                    break;
+                case 2:
+                    System.out.println("Guess the Number chosen");
+                    System.exit(0);
+                    break;
+                case 3:
+                    System.out.println("Quit");
+                    System.exit(0);
+                    break;
+            }
+            break;
+        }
+    }
+
 }
