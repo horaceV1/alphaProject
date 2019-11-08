0f611b18d839d662e7da5130582f013bcb2d2466
Daniel <danielfilipep18@gmail.com>
2019-11-08 14:34:51 -0100

    Added Server-side to the 'Server' class.

--- src/org/academiadecodigo/thunderstructs/Connections/Server.java
+++ src/org/academiadecodigo/thunderstructs/Connections/Server.java
@@ -1,7 +1,32 @@
 package org.academiadecodigo.thunderstructs.Connections;
 
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
+    private static final int PORT_NUMBER = 8080;
 
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(PORT_NUMBER);
 
+        while(true) {
+            Socket socket = serverSocket.accept();
+            BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(socket.getOutputStream());
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
+            String info;
+            while((info = bufferedReader.readLine()) != null) {
+                System.out.println(info);
+                if(info.equals("")) {
+                    break;
+                }
+            }
+
+            serverSocket.close();
+            socket.close();
+            bufferedOutputStream.close();
+            bufferedReader.close();
+        }
+    }
 }
